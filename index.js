e17deb41ad49ad644d706d5357a6cb6dc2b0a444
Nyakamba <omwegaenock@gmail.com>
2024-09-18 23:33:04 +0300

    create app APIs

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
 const express = require("express");
 const customers = require("./routes/customers");
 const genres = require("./routes/genres");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 
 const app = express();
 
@@ -15,6 +19,8 @@ mongoose
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/genres.js
+++ routes/genres.js
@@ -1,6 +1,6 @@
 const { Genre, validate } = require("../models/genre");
 const express = require("express");
-router = express.Router();
+const router = express.Router();
 
 router.get("/", async (req, res) => {
   const genres = await Genre.find().sort("name");
@@ -11,8 +11,8 @@ router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let genre = new Genre({ name: req.body.name });
-  genre = await genre.save();
+  const genre = new Genre({ name: req.body.name });
+  await genre.save();
 
   res.send(genre);
 });
