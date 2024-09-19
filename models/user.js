d358167d3cae78f0d894a605bf46f6f769ae4e38
Nyakamba <omwegaenock@gmail.com>
2024-09-19 11:46:09 +0300

    Role based authorization

--- models/user.js
+++ models/user.js
@@ -22,10 +22,17 @@ const userSchema = new mongoose.Schema({
     minlength: 5,
     maxlength: 1024,
   },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    process.env.JWT_SECRET
+  );
   return token;
 };
 
