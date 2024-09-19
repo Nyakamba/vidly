af15b7d4100c40d55ab71ffc18a13334f2df2ad1
Nyakamba <omwegaenock@gmail.com>
2024-09-19 16:21:18 +0300

    error handling

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
-const winston = require("winston");
-require("express-async-errors");
 require("dotenv").config();
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
-
-const mongoose = require("mongoose");
 const express = require("express");
-const customers = require("./routes/customers");
-const genres = require("./routes/genres");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const error = require("./middleware/error");
-
 const app = express();
-
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(() => console.log("Could not connect to MongoDB"));
-
-app.use(express.json());
-
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
+require("./startup/logging");
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/validate")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
